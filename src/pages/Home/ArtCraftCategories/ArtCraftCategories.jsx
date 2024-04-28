0ca1b33ceffa1d0d2c4d3bd576ff2a486bcc22e7
webdev-mostofa <mdmostofaa24@gmail.com>
2024-04-28 22:47:20 +0600

    some design change

---
+++ src/pages/Home/ArtCraftCategories/ArtCraftCategories.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const ArtCraftCategories = () => {
+  const [craftCategory, setCraftCategory] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/craftCategory")
+      .then((res) => res.json())
+      .then((data) => {
+        setCraftCategory(data);
+      });
+  }, []);
+  // console.log(craftCategory);
+  return (
+    <div className="grid grid-cols-1 py-5 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {craftCategory.map((singleCraft) => (
+        <Link to={`/craftCategory/${singleCraft?.subcategory}`}
+          key={singleCraft?._id}
+          className="flex hover:scale-105 transition-all hover:shadow-md-purple flex-col p-4 space-y-6  h-full  border-2 hover:border-[#00E661] border-gray-300 border-t-[#00E661] border-b-[#00e660d7] rounded-md  dark:bg-gray-50 dark:text-gray-800"
+        >
+          <div className="flex-grow">
+            <div className="rounded-lg">
+              <img
+                src={singleCraft?.image}
+                alt=""
+                className="object-cover rounded-md   w-full mb-4 h-52 md:h-60 mx-auto"
+              />
+            </div>
+
+            <h2 className="mb-1 text-xl font-bold">{singleCraft?.subcategory}</h2>
+          </div>
+          
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default ArtCraftCategories;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,27 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import SingleCraft from "../../components/SingleCraft/SingleCraft";
+import ArtCraftCategories from "./ArtCraftCategories/ArtCraftCategories";
 
 const Home = () => {
   const allCraft = useLoaderData();
-  console.log(allCraft);
+  // console.log(allCraft);
   return (
     <section>
       <Banner></Banner>
-      <div className="px-4 lg:px-12 ">
+      <div className="px-4 lg:px-12">
         <h2 className="lg:text-4xl text-3xl text-center mt-8 md:mb-3 md:mt-14 font-bold">Craft Items</h2>
         <div className="grid grid-cols-1 py-5 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
           {allCraft.slice(0, 6).map((craft) => (
             <SingleCraft craft={craft} key={craft._id}></SingleCraft>
           ))}
         </div>
+        <h2 className="lg:text-4xl text-3xl text-center mt-8 md:mb-8 md:mt-14 font-bold">Art & Craft Categories</h2>
+      <div>
+        <ArtCraftCategories></ArtCraftCategories>
       </div>
+      </div>
+      
       
     </section>
   );
